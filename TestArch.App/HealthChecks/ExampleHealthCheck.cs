45ad04dd283910a98ab662fa00066aba0c430849
Alex Mignonne <mignonne@live.ru>
2019-07-25 18:51:16 +0300

    Iteration

---
+++ TestArch.App/HealthChecks/ExampleHealthCheck.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace TestArch.App.HealthChecks
+{
+    public class ExampleHealthCheck :
+        IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken token = default)
+        {
+            var healthCheckResultHealthy = true;
+
+            return healthCheckResultHealthy
+                ? Task.FromResult(
+                    HealthCheckResult.Healthy(
+                        "The check indicates a healthy result."))
+                : Task.FromResult(
+                    HealthCheckResult.Unhealthy(
+                        "The check indicates an unhealthy result."));
+        }
+    }
+}
--- TestArch.App/Startup.cs
+++ TestArch.App/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using MediatR;
+﻿using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -8,7 +7,6 @@ using Microsoft.Extensions.DependencyInjection;
 using TestArch.Api;
 using TestArch.Domain;
 using TestArch.Infra;
-using TestArch.Shared;
 
 namespace TestArch.App
 {
@@ -21,7 +19,6 @@ namespace TestArch.App
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services
@@ -29,24 +26,29 @@ namespace TestArch.App
                     typeof(IApiAssembly).Assembly,
                     typeof(IDomainAssembly).Assembly
                 )
+                .AddHealthChecksUI()
                 .AddDomain()
                 .AddInfra()
+                .AddHealthCheckService()
                 .AddMvc()
                 .AddApplicationPart(typeof(IApiAssembly).Assembly)
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env)
         {
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
             else
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
 
-            app.UseHttpsRedirection();
-            app.UseMvc();
+            app
+                .UseHealthChecksUI(config => config.UIPath = "/hc-ui")
+                .AddHealthCheckConfigure()
+                .UseHttpsRedirection()
+                .UseMvc();
         }
     }
 }
